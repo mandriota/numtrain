860a2133dac6ebced3f3e657eccd48a0c570fbb8
Nameless Name <62650188+MarkMandriota@users.noreply.github.com>
2021-04-28 14:48:07 +0200

    Add files via upload

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 )
 
 var (
-	max = flag.Int64("max", 10, "")
+	maxn = flag.Int64("maxn", 10, "")
 )
 
 func init() {
@@ -22,20 +22,9 @@ func main() {
 }
 
 func setupUI() {
-	win := ui.NewWindow("trainer", 0, 0, true)
-	win.OnClosing(func(w *ui.Window) bool {
-		ui.Quit()
-		return true
-	})
-	win.SetMargined(true)
-
-	ui.OnShouldQuit(func() bool {
-		win.Destroy()
-		return true
-	})
+	win := mainWindows()
 
 	vb := ui.NewVerticalBox()
-
 	params := ui.NewGroup("Parameters")
 	form := ui.NewForm()
 
@@ -43,28 +32,27 @@ func setupUI() {
 	num := formSlider(form, "Shows number", 50)
 
 	params.SetChild(form)
-
 	vb.Append(params, true)
 
 	hb := ui.NewHorizontalBox()
 	number := ui.NewGroup("Number")
 	ovb := ui.NewVerticalBox()
 
-	outHand := &areaHandler{text: ""}
+	outHand := &areaHandler{text: "0"}
 	outArea := ui.NewArea(outHand)
 	outArea.Disable()
 
 	ovb.Append(outArea, true)
 
-	buttonGo := ui.NewButton("GO!")
 	sum := big.NewInt(0)
+	buttonGo := ui.NewButton("Go!")
 	buttonGo.OnClicked(func(b *ui.Button) {
 		go func() {
 			ticker := time.NewTicker(time.Duration(dur.Value()) * time.Millisecond)
 
 			sum.SetInt64(0)
 			for i := 0; i < num.Value(); i++ {
-				n, _ := rand.Int(rand.Reader, big.NewInt(*max))
+				n, _ := rand.Int(rand.Reader, big.NewInt(*maxn))
 				outHand.text = n.String()
 				outArea.QueueRedrawAll()
 				sum.Add(sum, n)
@@ -75,6 +63,7 @@ func setupUI() {
 			ticker.Stop()
 		}()
 	})
+
 	ovb.Append(buttonGo, true)
 	number.SetChild(ovb)
 	hb.Append(number, true)
@@ -87,21 +76,36 @@ func setupUI() {
 
 	buttonTest := ui.NewButton("Test")
 	buttonTest.OnClicked(func(b *ui.Button) {
-		if ansbox.Value() != int(sum.Int64()) {
-			ui.MsgBoxError(win, "Wrong", "Correct answer: "+sum.String())
-		} else {
+		switch ansbox.Value() {
+		case int(sum.Int64()):
 			ui.MsgBox(win, "Right", "Correct answer: "+sum.String())
+		default:
+			ui.MsgBoxError(win, "Wrong", "Correct answer: "+sum.String())
 		}
 	})
+
 	ivb.Append(buttonTest, true)
-
 	answer.SetChild(ivb)
-
 	hb.Append(answer, true)
 	vb.Append(hb, true)
-
 	win.SetChild(vb)
 	win.Show()
+}
+
+func mainWindows() *ui.Window {
+	win := ui.NewWindow("summation trainer", 0, 0, true)
+	win.OnClosing(func(w *ui.Window) bool {
+		ui.Quit()
+		return true
+	})
+	win.SetMargined(true)
+
+	ui.OnShouldQuit(func() bool {
+		win.Destroy()
+		return true
+	})
+
+	return win
 }
 
 func formSlider(form *ui.Form, label string, max int) (s *ui.Slider) {
@@ -117,17 +121,20 @@ type areaHandler struct {
 func (h *areaHandler) Draw(a *ui.Area, p *ui.AreaDrawParams) {
 	layout := ui.DrawNewTextLayout(&ui.DrawTextLayoutParams{
 		String:      ui.NewAttributedString(h.text),
-		DefaultFont: &ui.FontDescriptor{Family: "arial", Size: ui.TextSize(p.AreaHeight / 2), Weight: 20, Italic: 5, Stretch: 1},
+		DefaultFont: &ui.FontDescriptor{Family: "Arial", Size: ui.TextSize(p.AreaHeight / 2), Weight: 20, Italic: 5, Stretch: 1},
 		Width:       p.AreaWidth,
 	})
 
-	p.Context.Text(layout, 0, 0)
+	p.Context.Text(layout, p.AreaWidth/2, 0)
 	layout.Free()
 }
 
-func (areaHandler) MouseEvent(*ui.Area, *ui.AreaMouseEvent) {}
-func (areaHandler) MouseCrossed(*ui.Area, bool)             {}
-func (areaHandler) DragBroken(*ui.Area)                     {}
-func (areaHandler) KeyEvent(*ui.Area, *ui.AreaKeyEvent) bool {
+func (areaHandler) MouseEvent(a *ui.Area, e *ui.AreaMouseEvent) {}
+
+func (areaHandler) MouseCrossed(a *ui.Area, b bool) {}
+
+func (areaHandler) DragBroken(a *ui.Area) {}
+
+func (areaHandler) KeyEvent(a *ui.Area, k *ui.AreaKeyEvent) bool {
 	return false
 }
